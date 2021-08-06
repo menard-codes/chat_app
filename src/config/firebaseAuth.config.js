0cc839d169c53a4e5a2f72fd352054b5db94fa10
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2021-08-07 06:59:59 +0800

    twitter sign in

--- src/config/firebaseAuth.config.js
+++ src/config/firebaseAuth.config.js
@@ -2,7 +2,7 @@ export const uiconfig = firebase => ({
     signInFlow: 'redirect',
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID
     ],
     signInSuccessUrl: '/',
